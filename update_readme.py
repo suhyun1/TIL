b56e1dcfaf4c7eafb3193a9977b2ff108da36ac3
suhyun1 <suhyun7079@gmail.com>
2020-09-03 00:09:01 +0900

    feat: readme파일 자동 업데이트 스크립트 추가
    - 스크립트 실행하면 폴더, 파일 목록에 따라 리드미 자동 업데이트됨
    - 폴더는 알파벳순, 파일은 생성순으로 목록화

---
+++ update_readme.py
@@ -0,0 +1,43 @@
+import os
+import datetime
+
+HEADER = """# TIL
+> 배운 것을 기록하자
+
+"""
+
+ignoreFolders = [".git", ".github", "엔지니어를-위한-블록체인-프로그래밍"]
+
+
+def main():
+    content = ""
+    content += HEADER
+
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+
+        if root == ".":
+            for dir in ignoreFolders:
+                try:
+                    dirs.remove(dir)
+                except ValueError:
+                    pass
+            continue
+
+        folder = os.path.basename(root)
+        content += "### {}\n\n".format(folder)
+
+        files.sort(key=lambda f: os.path.getctime(folder + "/" + f))
+        for file in files:
+            if os.path.basename(file).split(".")[1] == "md":
+                name = os.path.basename(file).split(".")[0]
+                name = name.replace("-", " ")
+                content += "- [{}]({})\n".format(name, os.path.join(folder, file))
+        content += "\n"
+
+    with open("README.md", "w") as readme:
+        readme.write(content)
+
+
+if __name__ == "__main__":
+    main()
